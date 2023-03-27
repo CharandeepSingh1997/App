f12cd7612024ba06d26b20540b1d7014ab6db3e3
charan-code-lover <charan.saini.1997@gmail.com>
2023-03-26 23:08:43 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { LogBox } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/screens/home';
+import Profile from './src/screens/profile';
+import SignUpScreen from './src/screens/signup';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
+      <Stack.Screen name="Signup" component={SignUpScreen} options={{headerShown: false}}/>
+    </Stack.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
+  );
+};
+
+LogBox.ignoreAllLogs();
+
+export default App;
